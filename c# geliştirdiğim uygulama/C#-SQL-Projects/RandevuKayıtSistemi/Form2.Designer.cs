8cb52647c27449224b5418769220a07374e01207
Muhammed-Yusuf-Dinçer <myusufdincerr@gmail.com>
2026-01-13 17:08:32 +0300

    Add files via upload

---
+++ c# geliştirdiğim uygulama/C#-SQL-Projects/RandevuKayıtSistemi/Form2.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace uygulamam1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.btnAra);
+            this.groupBox1.Controls.Add(this.txtTC);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(344, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(846, 516);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kayıtlı Randevular";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(579, 177);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(232, 35);
+            this.btnSil.TabIndex = 22;
+            this.btnSil.Text = "Randevumu Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(290, 177);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(262, 35);
+            this.btnGuncelle.TabIndex = 21;
+            this.btnGuncelle.Text = "Randevumu Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 218);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(834, 292);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(25, 177);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(232, 35);
+            this.btnAra.TabIndex = 19;
+            this.btnAra.Text = "Randevumu Arat";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(166, 129);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(386, 29);
+            this.txtTC.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 24);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Tc Kimlik No:";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(206, 88);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(470, 29);
+            this.txtSoyad.TabIndex = 16;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(184, 41);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(470, 29);
+            this.txtAd.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 24);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "SoyAdınızı Giriniz:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 24);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Adınızı Giriniz:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1477, 640);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
---
+++ c# geliştirdiğim uygulama/C#-SQL-Projects/RandevuKayıtSistemi/Form2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulamam1
+{
+    public partial class Form2 : Form
+    {
+        MySqlConnection con = new MySqlConnection(
+        "Server=localhost;Database=randevu;Uid=root;Pwd=malatya44;Charset=utf8;");
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtAd.Text) ||
+                string.IsNullOrWhiteSpace(txtSoyad.Text) ||
+                string.IsNullOrWhiteSpace(txtTC.Text))
+            {
+                MessageBox.Show("Lütfen ad, soyad ve TC giriniz");
+                return;
+            }
+
+            MySqlDataAdapter da = new MySqlDataAdapter(
+                "SELECT Id, Ad, Soyad, TelNo, TCKimlik, DogumTarihi, RandevuTarihi " +
+                "FROM randevular WHERE Ad=@ad AND Soyad=@soyad AND TCKimlik=@tc", con);
+
+            da.SelectCommand.Parameters.AddWithValue("@ad", txtAd.Text);
+            da.SelectCommand.Parameters.AddWithValue("@soyad", txtSoyad.Text);
+            da.SelectCommand.Parameters.AddWithValue("@tc", txtTC.Text);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count == 0)
+            {
+                MessageBox.Show("Bu bilgilere ait randevu bulunamadı");
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Lütfen silmek için bir randevu seçiniz");
+                return;
+            }
+
+            DialogResult sonuc = MessageBox.Show(
+                "Seçilen randevuyu silmek istiyor musunuz?",
+                "Onay",
+                MessageBoxButtons.YesNo);
+
+            if (sonuc != DialogResult.Yes)
+                return;
+
+            int id = Convert.ToInt32(
+                dataGridView1.CurrentRow.Cells["Id"].Value);
+
+            MySqlCommand cmd = new MySqlCommand(
+                "DELETE FROM randevular WHERE Id=@id", con);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Randevu silindi");
+
+            // Grid'i yenile
+            btnAra_Click(null, null);
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {    
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {      
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Lütfen güncellenecek randevuyu seçiniz");
+                return;
+            }
+
+            Form3 frm = new Form3();
+
+            frm.id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["Id"].Value);
+            frm.Ad = dataGridView1.CurrentRow.Cells["Ad"].Value.ToString();
+            frm.Soyad = dataGridView1.CurrentRow.Cells["Soyad"].Value.ToString();
+            frm.TelNo = dataGridView1.CurrentRow.Cells["TelNo"].Value.ToString();
+            frm.TCKimlik = dataGridView1.CurrentRow.Cells["TCKimlik"].Value.ToString();
+            frm.DogumTarihi = Convert.ToDateTime(dataGridView1.CurrentRow.Cells["DogumTarihi"].Value);
+            frm.RandevuTarihi = Convert.ToDateTime(dataGridView1.CurrentRow.Cells["RandevuTarihi"].Value);
+
+            frm.Show();
+            this.Hide();
+        }
+
+    }
+}
